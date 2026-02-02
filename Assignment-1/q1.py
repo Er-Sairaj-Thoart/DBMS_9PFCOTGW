25e219f5cd608e0b9d532a09babeda03879d7591
Er-Sairaj-Thoart <sairajthorat1221@gmail.com>
2026-02-02 15:59:50 +0530

    Init: Assignment 1

--- Assignment-1/q1.py
+++ Assignment-1/q1.py
@@ -1,11 +1,9 @@
-s = input()
-d={"UPPER CASE":0, "LOWER CASE":0}
-for c in s:
-    if c.isupper():
-        d["UPPER CASE"]+=1
-    elif c.islower():
-        d["LOWER CASE"]+=1
-    else:
-        pass
-print("UPPER CASE", d["UPPER CASE"])
-print("LOWER CASE", d["LOWER CASE"])
+# q1.py
+
+"""
+Question: Write a program that accepts a sentence and calculate the number of letters and digits. Suppose the following input is supplied to the program: hello world! 123 Then, the output should be: LETTERS 10 DIGITS 3
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+# Start coding below...
